daead36b6d9fb0614fca966e2345279acc0dc519
harshit1142 <harshitpandey98765@gmail.com>
2024-07-03 00:00:17 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
